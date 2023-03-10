2bc13d9d3c047c7130111b830c13049f7d759563
fabiengoyhex <fabien.goyhex@gmail.com>
2023-03-10 15:53:50 +0100

    added data into the template

--- pizzaMama/menu/views.py
+++ pizzaMama/menu/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pizza
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('les pizzas de ta mere')
+    pizzas = Pizza.objects.all()
+    return render(request, 'menu/index.html', {'my_pizzas_key': pizzas})
+    '''pizzas = Pizza.objects.all()
+    pizzas_names_and_price = [pizza.name + " : " +
+                              str(pizza.price) + " €" for pizza in pizzas]
+    pizzas_names_and_Price_str = ", ".join(pizzas_names_and_price)
+
+    return HttpResponse("Nos pizza : " + pizzas_names_and_Price_str)'''
